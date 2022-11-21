6218997c6c44ee7aeaccec9d8dd6d7bbe2320d77
ThomasMcDonald904 <naynaytom3000@gmail.com>
2022-11-21 16:47:55 -0500

    Nettoyage constantes

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ from networktables.util import ntproperty
 
 class Ports:
     """
-    Convention : sous_systeme + _ + composante
+    Convention : ( sous-système ) + _ + ( composante )
     """
 
     # CAN
@@ -20,15 +20,10 @@ class Ports:
 
 
 class _Proprietes:
-    dropReturnTime = ntproperty("/Proprietes/Trois Dents/DropReturnTime", 1, writeDefault=False)
-    dropResetTime = ntproperty("/Proprietes/Trois Dents/DropResetTime", 1, writeDefault=False)
     pilotage_max_x = ntproperty("/Proprietes/PilotageMaxX", 0.4, writeDefault=False)
     pilotage_max_y = ntproperty("/Proprietes/PilotageMaxY", 0.4, writeDefault=False)
     pilotage_max_z = ntproperty("/Proprietes/PilotageMaxZ", 0.2, writeDefault=False)
     pilotage_deadzone = ntproperty("/Proprietes/PilotageDeadzone", 0.05, writeDefault=False)
-    aligner_max_speed = ntproperty("/Proprietes/AlignerMaxSpeed", 0.15, writeDefault=False)
-    aligner_error_multiplier = ntproperty("/Proprietes/AlignerErrorMultiplier", 1.5, writeDefault=False)
-    aller_max_speed = ntproperty("/Proprietes/AllerMaxSpeed", 0.1, writeDefault=False)
-    aligner_offset = ntproperty("/Proprietes/AlignerOffset", 0.25, writeDefault=True)
+
 
 Proprietes = _Proprietes()
