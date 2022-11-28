636388b47445cb85ca73e0c2defe1db3a691a537
Felix Albert <filalbert16@outlook.com>
2022-11-28 17:03:48 -0500

    AvancerX tester

--- commands/avancerx.py
+++ commands/avancerx.py
@@ -1,22 +1,26 @@
+import math
+
 from subsystems.basepilotable import BasePilotable
 from utils.safecommandbase import SafeCommandBase
 
 class AvancerX(SafeCommandBase):
-    def __init__(self, base_pilotable: BasePilotable, distance, speed):
+    def __init__(self, base_pilotable: BasePilotable, distance, vitesse):
         super().__init__()
 
         self.base_pilotable = base_pilotable
         self.distance = distance
-        self.speed = speed
+        self.vitesse = vitesse
+        self.erreur = math.inf
 
     def initialize(self):
         self.base_pilotable.resetOdometry()
 
     def execute(self):
-        self.base_pilotable.driveCartesian(self.speed, self.speed, 0)
+        self.erreur = self.distance - sum(self.base_pilotable.getEncoderDistances())/4
+        self.base_pilotable.driveCartesian(self.vitesse, 0, 0)
 
     def end(self, interrupted: bool):
         self.base_pilotable.driveCartesian(0, 0, 0)
 
     def isFinished(self) -> bool:
-        return sum(self.base_pilotable.getEncoderDistances())/4 >= self.distance
+        return self.erreur <= 0.05
--- robot.py
+++ robot.py
@@ -5,6 +5,7 @@ import wpilib
 
 from commands.piloter import Piloter
 from commands.tourner import Tourner
+from commands.avancerx import AvancerX
 from subsystems.basepilotable import BasePilotable
 from commands2.button import JoystickButton
 
@@ -18,7 +19,7 @@ class Robot(commands2.TimedCommandRobot):
         self.xbox_controller = wpilib.Joystick(1)
 
         self.base_pilotable.setDefaultCommand(Piloter(self.base_pilotable, self.stick, self.xbox_controller))
-        JoystickButton(self.stick, 3).whenPressed((Tourner(self.base_pilotable, 90, 1)))
+        JoystickButton(self.stick, 5).whenPressed((AvancerX(self.base_pilotable, 1, 1000)))
         JoystickButton(self.stick, 4).whenPressed((Tourner(self.base_pilotable, -90, 1)))
 
 
