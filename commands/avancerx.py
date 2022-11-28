b708a1e940ab66df478af621226beffcb20a42a0
Felix Albert <filalbert16@outlook.com>
2022-11-28 17:22:40 -0500

    Rajout d'angles AvancerX (fancy)

--- commands/avancerx.py
+++ commands/avancerx.py
@@ -4,20 +4,21 @@ from subsystems.basepilotable import BasePilotable
 from utils.safecommandbase import SafeCommandBase
 
 class AvancerX(SafeCommandBase):
-    def __init__(self, base_pilotable: BasePilotable, distance, vitesse):
+    def __init__(self, base_pilotable: BasePilotable, distance, vitesse, angle):
         super().__init__()
 
         self.base_pilotable = base_pilotable
         self.distance = distance
-        self.vitesse = vitesse
+        self.vitesseX = distance * math.cos(math.radians(angle)) * vitesse
+        self.vitesseY = distance * math.sin(math.radians(angle)) * vitesse
         self.erreur = math.inf
 
     def initialize(self):
         self.base_pilotable.resetOdometry()
 
     def execute(self):
-        self.erreur = self.distance - sum(self.base_pilotable.getEncoderDistances())/4
-        self.base_pilotable.driveCartesian(self.vitesse, 0, 0)
+        self.erreur = self.distance - abs(sum(self.base_pilotable.getEncoderDistances())/4)
+        self.base_pilotable.driveCartesian(self.vitesseX, self.vitesseY, 0)
 
     def end(self, interrupted: bool):
         self.base_pilotable.driveCartesian(0, 0, 0)
