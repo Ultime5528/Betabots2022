f3d46ecbb97081ca4d84bc8053e8da6de69ac5fe
Maxence <3800945@keranna.qc.ca>
2022-12-02 14:57:40 -0500

    Fix mode auto

--- commands/auto4cubes.py
+++ commands/auto4cubes.py
@@ -1,39 +1,38 @@
 from subsystems.basepilotable import BasePilotable
 from subsystems.tireur import Tireur
-from commands.avancer import Avancer
+from commands.avancerx import AvancerX
 from commands.tourner import Tourner
 from commands.tirerproche import TirerProche
 import commands2
 
+
 class Auto4Cubes(commands2.SequentialCommandGroup):
     def __init__(self, base_pilotable: BasePilotable, tireur: Tireur):
+        self.setName(Auto4Cubes)
         super().__init__(
             commands2.SequentialCommandGroup(
                 commands2.WaitCommand(0.5),
-                #avancer: dist. x, dist. y, vitesse
-                #tourner: angle, vitesse
+                # avancer: dist. x, dist. y, vitesse
+                # tourner: angle, vitesse
 
-                #aller chercher les cubes:
-                Avancer(base_pilotable, -46, 0, -0.5),
-                Avancer(base_pilotable, 0, 66, 0.5),
-                Avancer(base_pilotable, 60, 0, 0.5),
+                # aller chercher les cubes:
+                AvancerX(base_pilotable, -46, 0, -0.5),
+                AvancerX(base_pilotable, 0, 66, 0.5),
+                AvancerX(base_pilotable, 60, 0, 0.5),
                 Tourner(base_pilotable, 90, 0.5),
-                Avancer(base_pilotable, 130, 0, 0.5),
+                AvancerX(base_pilotable, 130, 0, 0.5),
                 Tourner(base_pilotable, 90, 0.5),
-                Avancer(base_pilotable, 60, 0, 0.5),
+                AvancerX(base_pilotable, 60, 0, 0.5),
 
-                #aller porter les cubes vers l'accumulateur:
-                Avancer(base_pilotable, 265, 0, 0.5),
-                #placer les cubes:
-                Avancer(base_pilotable, 0, 30, 0.5),
+                # placer les cubes:
+                AvancerX(base_pilotable, 265, 30, 0.5),
                 TirerProche(tireur),
-                Avancer(base_pilotable, 0, 15, 0.5),
+                AvancerX(base_pilotable, 0, 15, 0.5),
                 TirerProche(tireur),
-                Avancer(base_pilotable, 0, 15, 0.5),
+                AvancerX(base_pilotable, 0, 15, 0.5),
                 TirerProche(tireur),
-                Avancer(base_pilotable, 0, 15, 0.5 ),
+                AvancerX(base_pilotable, 0, 15, 0.5),
                 TirerProche(tireur),
 
             ),
         )
-        self.setName(Auto4Cubes)
